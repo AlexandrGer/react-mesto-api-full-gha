ad17be66b223ceba4f8bc944c60095a099598698
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2023-09-14 12:11:20 +0300

    Практическая работа 15

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const router = require('./routes/index');
+const routerAuth = require('./routes/auth');
+const auth = require('./middlewares/auth');
+const handleError = require('./errors/handleError');
+const cors = require('cors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const {
+  PORT = 3000,
+  DB_URL = 'mongodb://127.0.0.1:27017/mestodb',
+} = process.env;
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+});
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(requestLogger);
+
+app.use('/', routerAuth);
+app.use(auth);
+app.use(router);
+
+app.use(errorLogger);
+app.use(errors());
+app.use(handleError);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ backend/utils/constants.js
@@ -0,0 +1,8 @@
+const SECRET_KEY = 'some-secret-key';
+
+// const URL_REG = /^(http|https):\/\/(www\.)?[a-zA-Z0-9\--._~:/?#[\]@!$&'()*+,;=]+#?$/;
+
+module.exports = {
+  SECRET_KEY,
+  // URL_REG,
+};
